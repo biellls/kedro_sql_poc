fcda0c8dbdccefdafbb5434a2f9642c6780d06cb
biellls <bielllobera@gmail.com>
2022-12-03 12:35:36 +0100

    Small improvements README

--- src/kbt/pipelines/data_processing/pipeline.py
+++ src/kbt/pipelines/data_processing/pipeline.py
@@ -83,18 +83,18 @@ def create_pipeline(**kwargs) -> Pipeline:
     ]
     return pipeline(
         [
-            # node(
-            #     func=preprocess_companies,
-            #     inputs="companies",
-            #     outputs="preprocessed_companies",
-            #     name="preprocess_companies_node",
-            # ),
-            # node(
-            #     func=preprocess_shuttles,
-            #     inputs="shuttles",
-            #     outputs="preprocessed_shuttles",
-            #     name="preprocess_shuttles_node",
-            # ),
+            node(
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="preprocessed_companies",
+                name="preprocess_companies_node",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="preprocessed_shuttles",
+                name="preprocess_shuttles_node",
+            ),
             *nodes
         ]
     )
